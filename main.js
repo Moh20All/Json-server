d6cbffb0209aee10dfcaa209266608138ee1289c
Moh20All <89274884+Moh20All@users.noreply.github.com>
2023-08-26 19:05:54 +0100

    it is finished

--- main.js
+++ main.js
@@ -1,32 +1,30 @@
 //fetch
-fetch("https://randomuser.me/api/?results=5")
+fetch("http://127.0.0.1:5500/database/user.json")
     .then(res=>res.json())
-    .then(json=>{
-        for (const key in json.results) {
-  if ((json.results).hasOwnProperty(key)) {
-    appendEl(json.results[key]);
-    console.log(json.results[key]);
-        }
-    }})
+    .then(data=> {
+      data.forEach(ele => {
+        appendEl(ele);
+      });
+    })
 
 
 // append function
-function appendEl({name,picture,email,location,phone}){
+function appendEl({Name,lastName,avatar,email,gender,job,Busy}){
     let body = document.getElementById("tbody");
     
     let li = document.createElement("li");
 
     li.innerHTML = `<li class="flex justify-between gap-x-6 py-5">
     <div class="flex min-w-0 gap-x-4">
-      <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="${picture.medium}" alt="">
+      <img class="h-12 w-12 flex-none rounded-full bg-gray-50 object-cover" src="${avatar}" alt="">
       <div class="min-w-0 flex-auto">
-        <p class="text-sm font-semibold leading-6 text-gray-900">${name.first+" "+name.last}</p>
+        <p class="text-sm font-semibold leading-6 text-gray-900">${Name+" "+lastName}</p>
         <p class="mt-1 truncate text-xs leading-5 text-gray-500">${email}</p>
       </div>
     </div>
     <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-      <p class="text-sm leading-6 text-gray-900">${phone}</p>
-      <p class="mt-1 text-xs leading-5 text-gray-500">lives in ${location.city}</time></p>
+      <p class="text-sm leading-6 text-gray-900">${job}</p>
+      <p class="mt-1 text-xs leading-5 text-gray-500">${(Busy.online)?"Online":`Last Seen ${Busy.LastSeen}`}</p>
     </div>
   </li>`
   body.append(li);
